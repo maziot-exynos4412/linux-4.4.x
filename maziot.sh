c91b5ff863c62b9ff50128772192a5bd22536018
mz8023yt <mz8023yt@163.com>
2018-04-08 22:47:15 +0800

    feature: update the maziot.sh

--- maziot.sh
+++ maziot.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
+
+make -j24 uImage LOADADDR=0x40008000
+
+echo ""
+echo "\033[31m>>>>>>>>>>>>>>>>>>>>>>>>>>>> make uImage successful <<<<<<<<<<<<<<<<<<<<<<<<<<<<\033[0m"
+echo ""
+
+make dtbs
+
+echo ""
+echo "\033[31m>>>>>>>>>>>>>>>>>>>>>>>>>>>>> make dtbs successful <<<<<<<<<<<<<<<<<<<<<<<<<<<<<\033[0m"
+echo ""
+
 cp -av arch/arm/boot/dts/exynos4412-tiny4412.dtb ./
 cp -av arch/arm/boot/uImage ./
+
+echo ""
+echo "\033[31m>>>>>>>>>>>>>>>>>>>>>>>> copy uImage and dtb successful <<<<<<<<<<<<<<<<<<<<<<<<\033[0m"
+echo ""
+
